00e531bdd70d26a08342ef30fb841f9d13195725
Baek-Donghyeon <nistring@naver.com>
2025-03-04 11:14:01 +0900

    Update test script and add installation instructions

---
+++ install.md
@@ -0,0 +1,7 @@
+python <= 3.11
+Install PyTorch following official instructions
+pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu118
+pip install -U openmim
+mim install mmengine
+pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.1/index.html
+pip install "numpy<2"
--- tools/test.sh
+++ tools/test.sh
@@ -2,10 +2,15 @@ strings=("fidgety17" "fidgety29" "writhing17" "writhing29")
 epoch=20
 for str in "${strings[@]}"
 do
-    for cv in {6..10}
+    for cv in {1..10}
     do
-    python tools/test.py configs/skeleton/gm/stgcn_2xb8-joint-${str}-keypoint-2d.py \
-        work_dirs/${str}/${cv}/epoch_${epoch}.pth \
+    bash tools/dist_test.sh configs/skeleton/gm/stgcn_2xb8-joint-${str}-keypoint-2d.py \
+        work_dirs/${str}/${cv}/epoch_${epoch}.pth 4 \
         --dump work_dirs/${str}/model_${cv}_e${epoch}.pkl
     done
 done
+#     python tools/test.py configs/skeleton/gm/stgcn_2xb8-joint-${str}-keypoint-2d.py \
+#         work_dirs/${str}/${cv}/epoch_${epoch}.pth \
+#         --dump work_dirs/${str}/model_${cv}_e${epoch}.pkl
+#     done
+# done
